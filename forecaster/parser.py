e92c7edc7006d6edf24566e0cc23f9f4a1e32554
Rogerio Chaves <rogeriocfj@gmail.com>
2021-04-25 14:22:46 +0200

    Also extracts hourly forecasts

--- forecaster/parser.py
+++ forecaster/parser.py
@@ -11,19 +11,20 @@ def extract_10_day_forecast(html, today=today()):
     daily_forecast = soup.find(attrs=test_id("DailyForecast"))
     summaries = daily_forecast.find_all("summary")
 
-    date = today
+    datetime = start_of_yyyy_mm_dd(today)
     result = []
     for summary in summaries:
-        item = extract_day_forecast(summary, date)
-        date = move_yyyy_mm_dd(date, 1)
+        item = extract_day_forecast(summary, datetime)
+        datetime = move_yyyy_mm_dd(datetime, 1)
         result.append(item)
 
     return result
 
 
-def extract_day_forecast(summary, date):
+def extract_day_forecast(summary, datetime):
     item = {
-        "date": date,
+        "type": "daily",
+        "datetime": datetime,
         "precipitation": "0%",
         "summary": summary.find(attrs=test_id("wxIcon")).find("span").get_text()
     }
@@ -32,6 +33,43 @@ def extract_day_forecast(summary, date):
         attrs=test_id("TemperatureValue")).get_text()
     item["min"] = temperature.find(attrs=test_id(
         "lowTempValue")).find("span").get_text()
+
+    precipitation = summary.find(attrs=test_id("Precip"))
+    if precipitation:
+        item["precipitation"] = precipitation.find(
+            attrs=test_id("PercentageValue")).get_text()
+
+    return item
+
+
+def extract_hourly_forecast(html, now=now()):
+    soup = BeautifulSoup(html, features='lxml')
+    daily_forecast = soup.find(attrs=test_id("HourlyForecast"))
+    summaries = daily_forecast.find_all("summary")
+
+    datetime = move_yyyy_mm_dd_hh(start_of_yyyy_mm_dd(now), hour(now))
+    # if it's quarter past the hour TWC shows forecast for next hour
+    if to_datetime(now).minute > 15:
+        datetime = move_yyyy_mm_dd_hh(datetime, 1)
+    result = []
+    for summary in summaries:
+        item = extract_hour_forecast(summary, datetime)
+        datetime = move_yyyy_mm_dd_hh(datetime, 1)
+        result.append(item)
+
+    return result
+
+
+def extract_hour_forecast(summary, datetime):
+    item = {
+        "type": "hourly",
+        "datetime": datetime,
+        "precipitation": "0%",
+        "summary": summary.find(attrs=test_id("wxIcon")).find("span").get_text()
+    }
+    temperature = summary.find(attrs=test_id("detailsTemperature"))
+    item["temperature"] = temperature.find(
+        attrs=test_id("TemperatureValue")).get_text()
 
     precipitation = summary.find(attrs=test_id("Precip"))
     if precipitation:
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -14,17 +14,40 @@ class ParserTestCase(unittest.TestCase):
             fixture("ams-ten-day.html"), today='2021-04-25')
         self.assertEqual(result[0:2], [
             {
-                'date': '2021-04-25',
+                'type': 'daily',
+                'datetime': '2021-04-25T00:00:00',
                 'precipitation': '0%',
                 'summary': 'AM Clouds/PM Sun',
                 'max': '10°',
                 'min': '2°'
             },
             {
-                'date': '2021-04-26',
+                'type': 'daily',
+                'datetime': '2021-04-26T00:00:00',
                 'precipitation': '10%',
                 'summary': 'Partly Cloudy',
                 'max': '11°',
                 'min': '3°'
+            },
+        ])
+
+    def test_extracts_hourly_forecast(self):
+        result = parser.extract_hourly_forecast(
+            fixture("ams-hourly.html"), now='2021-04-25T15:01:00')
+
+        self.assertEqual(result[0:2], [
+            {
+                'type': 'hourly',
+                'datetime': '2021-04-25T15:00:00',
+                'precipitation': '0%',
+                'summary': 'Mostly Cloudy',
+                'temperature': '10°'
+            },
+            {
+                'type': 'hourly',
+                'datetime': '2021-04-25T16:00:00',
+                'precipitation': '0%',
+                'summary': 'Mostly Cloudy',
+                'temperature': '10°'
             },
         ])
